b50a2988734c8030f213f47b2a729d950d449802
Davi Felix <davifelix@usp.br>
2024-01-22 09:03:31 -0300

    primeira implementação (um passo explícitos)

---
+++ NumericalMethod.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+Classe para implementar e avaliar um método numérico qualquer
+"""
+class NumericalMethod:
+    def __init__(self, T: float, y_0: float, f: callable, n: int = 1000, t_0: float = 0):
+        """
+        n: número de pontos na partição uniforme de discretização
+        t_0: tempo inicial
+        T: tempo final
+        y_0: condição incial da variável de estado
+        f: função f do problema de Cauchy
+        """
+        self.T = T
+        self.t_0 = t_0
+        self.f = f
+        self.n = self.n0 = n
+        self.y_0 = y_0
+
+    def run(self):
+        """
+        Função que executa o método numérico
+        """
+        raise NotImplementedError
+
+    # Definindo a propriedade n e seu setter para que o h seja redefinido quando n é atualizado
+    @property
+    def n(self):
+        return self._n
+
+    @n.setter
+    def n(self, value: int):
+        self._n = value
+        self.h = (self.T-self.t_0)/value
+
+    def convergence_table_with_exact_solution(self, r: int, n_0: int, n_loops: int, exact_value_on_T: float):
+        """
+        Constrói uma tabela de convergência com a ordem de convergência estimada a partir do valor exato da solução.
+        r: razão de refinamento
+        n_0: valor de n na primeira aplicação do método
+        n_loops: quantidade de aplicações do método para a tabela
+        exact_value_on_T: valor da solução exata no instante de tempo final
+        """
+        i = 0
+        n_values = []
+        h_values = []
+        global_errors = []
+        logs = [0]
+        for i in range(n_loops):
+            self.n = n_0*r**(i)
+            n_values.append(self.n)
+            h_values.append(self.h)
+            self.run()
+            y_n = self.y[-1]
+            global_errors.append(np.abs(y_n - exact_value_on_T))
+            if i >= 1:
+                q = abs(global_errors[i-1]/global_errors[i])
+                logs.append(np.log(q)/np.log(r))
+        return list(zip(n_values, h_values, global_errors, logs))
+    
+    def convergence_table_without_exact_solution(self, r: int, n_0: int, n_loops: int):
+        """
+        Constrói uma tabela de convergência com a ordem de convergência estimada numericamente sem a solução exata.
+        r: razão de refinamento
+        n_0: valor de n na primeira aplicação do método
+        n_loops: quantidade de linhas na tabela
+        exact_value_on_T: valor da solução exata no instante de tempo final
+        """
+        i = 0
+        n_values = []
+        h_values = []
+        numerical_solutions = []
+        logs = [0, 0]
+        for i in range(n_loops):
+            self.n = n_0*r**(i)
+            n_values.append(self.n)
+            h_values.append(self.h)
+            self.run()
+            numerical_solutions.append(self.y[-1])
+            if i >= 2:
+                q = abs((numerical_solutions[i-2]-numerical_solutions[i-1])/(numerical_solutions[i-1]-numerical_solutions[i]))
+                logs.append(np.log(q)/np.log(r))
+        return list(zip(n_values, h_values, numerical_solutions, logs))
+    
+    def plot_solutions(self, exact_function: callable=None):
+        """
+        Função que plota a solução numérica encontrada pelo método. Caso seja fornecida uma solução exata,
+        ela também é plotada com sua respetiva leganda.
+        """
+        self.n = self.n0  # Volta ao intervalo de discretização original
+        self.run()
+        plt.title("Solução encontrada para a equação")
+        plt.ylabel("y(t)")
+        plt.xlabel("t")
+        plt.plot(self.t, self.y, label="y(t)")
+        if exact_function:
+            x = np.linspace(self.t_0, self.T, 1000)
+            plt.plot(x, exact_function(x), label="$y_e(t)", color='red', linestyle='--', alpha=0.6)
+            plt.legend()
+        plt.show()
+
+    def plot_steps(self, n_0: int, r: int, n_loops: int):
+        """
+        Plota a aproximação numérica para diferentes valores de n 
+        n_0: valor inicial de n
+        r: razão com a qual n aumenta
+        n_loops: quantidade de plots e aproximações a serem feitas
+        """
+        plt.figure(figsize=(10,8))
+        plt.title("Sucessivas aproximações numéricas para diferentes passos de integração")
+        plt.ylabel("y(t)")
+        plt.xlabel("t")
+        for i in range(n_loops):
+            n = n_0*(r**i)
+            self.n = n
+            self.run()
+            plt.plot(self.t, self.y, label=f'n={i}')
+        plt.legend()
+        plt.show()
+
+
+def convert_dict_to_latex(convergence_data: list):
+    """
+    Função para converter a tabela de convergência em notação do Latex
+    convergence_data: dados retornados pelo método da tabela de convergência
+    """
+    for value in convergence_data:
+        print('{:5d} & {:.3e} & {:.3e} & {:.3e} \\\\'.format(*value))
---
+++ OneStepMethod.py
@@ -0,0 +1,106 @@
+"""           
+Implementação de um método de passo único explícito genérico para 
+uma variável de estado (com exemplos)
+------------------------------------------------------------------
+ Revisoes  :
+     Data        Versao  Autor             Descricao
+     21/01/2023  1.0     Davi Félix        versao inicial
+------------------------------------------------------------------
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from NumericalMethod import NumericalMethod, convert_dict_to_latex
+
+"""
+Classe genérica para um método explícito de passo único capaz de resolver uma variável de estado
+"""
+class OneStepMethod(NumericalMethod):
+
+    def phi(self, t: float, y: float):
+        """
+        Função Phi do método de passdo único explícito
+        """
+        raise NotImplementedError
+
+    def run(self):
+        """
+        Função que executa o laço tempora do Método
+        """
+        self.y = [self.y_0]
+        self.t = [self.t_0]
+        for k in range(self.n):
+            self.y.append(self.y[k] + self.h*self.phi(self.t[k], self.y[k]))
+            self.t.append(self.t[k] + self.h)
+
+
+class EulerMethod(OneStepMethod):
+    """
+    Método de Euler simples
+    """
+    def phi(self, t, y):
+        return self.f(t, y)
+    
+
+class EulerMethod2(OneStepMethod):
+    """
+    Método de Euler modificado
+    """
+    def phi(self, t, y):
+        return 0.5*(self.f(t, y) + self.f(t + self.h, y + self.h*self.f(t, y)))
+    
+
+class RKC(OneStepMethod):
+    """
+    Método de Runge Kutta Clássico
+    """
+    def phi(self, t, y):
+        k1 = self.f(t, y)
+        k2 = self.f(t+self.h/2, y + self.h/2*k1)
+        k3 = self.f(t+self.h/2, y + self.h/2*k2)
+        k4 = self.f(t+self.h, y + self.h*k3)
+    
+        return 1/6*(k1 + 2*k2 + 2*k3 + k4)
+
+
+if __name__ == '__main__':
+    ######## EXEMPLO 1 - EX 2.2 DA SEÇÃO 2.4 DAS NOTAS DE AULA ##########
+    y_e = lambda t: np.cos(t) - np.sin(t)
+    T = 1
+    y_0 = 1
+    euler = EulerMethod(T, y_0, lambda t, y: -y*np.tan(t) - 1/np.cos(t))
+    print('Tabela de convergência com solução exata de 2.2 em T=1')
+    convert_dict_to_latex(
+        euler.convergence_table_with_exact_solution(r=2, n_0=64, n_loops=9, exact_value_on_T=y_e(T))
+    )
+    print('\nTabela de convergência com solução numérica de 2.2 em T=1')
+    convert_dict_to_latex(
+        euler.convergence_table_without_exact_solution(r=3, n_0=64, n_loops=9)
+    )
+    euler.plot_solutions(exact_function=y_e)
+
+    ######### EXEMPLO 2 - EXEXMPLO 2.4 DA SEÇÃO 2.3;1 DAS NOTAS DE AULA ##############
+
+    y_e = lambda t: np.sin(2*np.pi*t)*np.exp(-0.2*t)
+    T = 1/2
+    y_0 = 0
+    euler = EulerMethod(T, y_0, lambda t, y: 2*np.pi*np.cos(2*np.pi*t)*np.exp(-0.2*t) - 0.2*y)
+    print('\nTabela de convergência com solução exata de 2.4 em T=1/2')
+    convert_dict_to_latex(euler.convergence_table_with_exact_solution(r=2, n_0=64, n_loops=4, exact_value_on_T=y_e(T)))
+    euler.T = 1  # Mudando o instante de tempo final para 1
+    print('\nTabela de convergência com solução exata de 2.4 em T=1')
+    convert_dict_to_latex(euler.convergence_table_with_exact_solution(r=2, n_0=64, n_loops=4, exact_value_on_T=y_e(T)))
+    
+    ######### EXEMPLO 3 - Equação manufaturada ###################
+    y_e = lambda t: 3*(t**2)*np.cos(t)*np.exp(-2*t)
+    T = 5
+    y_0 = 0
+    rkc = RKC(T, y_0, lambda t, y: -3*np.exp(-2*t)*t*(t*np.sin(t)+2*(t-1)*np.cos(t)))
+    print('\nTabela de convergêmcia para o exemplo 3 em T=5')
+    convert_dict_to_latex(rkc.convergence_table_with_exact_solution(
+        r=2, n_0=16, n_loops=8, exact_value_on_T=y_e(T)
+    ))
+    rkc.plot_solutions(exact_function=y_e)
+   
+    # Plot para soluções usando diferentes valores de n
+    rkc.plot_steps(n_0=2, r=2, n_loops=9)
