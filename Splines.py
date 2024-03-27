ee5124caeed8564855d68b189ca94c1856a76d94
Davi Felix <davifelix@usp.br>
2024-03-27 02:35:00 -0300

    :wrench: preparando para splines

--- Splines.py
+++ Splines.py
@@ -16,29 +16,32 @@ class Spline:
         self.u = np.zeros((self.n+1,))
         self.z = np.zeros((self.n+1,))
         self.alphas = np.zeros((self.n+1,))
-        self.h = h
+        self.h = np.zeros((self.n,))
 
+    def calculate_h(self):
+        for i in range(self.n):
+            self.h[i] = self.x[i+1] - self.x[i]
 
     def calculate_alphas(self):
-        self.alphas[0] = (3*(self.a[1] - self.a[0]))/self.h - 3*self.fpo
-        self.alphas[self.n] = 3*self.fpn - (3*(self.a[self.n] - self.a[self.n - 1]))/self.h
+        self.alphas[0] = (3*(self.a[1] - self.a[0]))/self.h[0] - 3*self.fpo
+        self.alphas[self.n] = 3*self.fpn - (3*(self.a[self.n] - self.a[self.n - 1]))/self.h[self.n-1]
         for i in range(1, self.n):
-            self.alphas[i] = (3*(self.a[i + 1] - self.a[i]))/self.h  -  (3*(self.a[i] - self.a[i - 1]))/self.h
+            self.alphas[i] = (3*(self.a[i + 1] - self.a[i]))/self.h[i]  -  (3*(self.a[i] - self.a[i - 1]))/self.h[i-1]
 
     def solve_linear_system(self):
-        self.l[0] = 2*self.h
+        self.l[0] = 2*self.h[0]
         self.u[0] = 0.5
         self.z[0] = self.alphas[0]/self.l[0]
 
         for i in range(1, self.n):
-            self.l[i] = 2*(self.x[i + 1] -  self.x[i - 1]) - self.h*self.u[i - 1]
+            self.l[i] = 2*(self.x[i + 1] -  self.x[i - 1]) - self.h[i-1]*self.u[i - 1]
             
-            self.u[i] = self.h/self.l[i]
+            self.u[i] = self.h[i]/self.l[i]
 
-            self.z[i] = (self.alphas[i] - (self.h*self.z[i-1]))/self.l[i]
+            self.z[i] = (self.alphas[i] - (self.h[i-1]*self.z[i-1]))/self.l[i]
         
-        self.l[self.n] = (self.h*(2 - self.u[self.n - 1])) # l_n
-        self.z[self.n] = (self.alphas[self.n] - self.h*self.z[self.n - 1])/self.l[self.n] # z_n
+        self.l[self.n] = (self.h[self.n-1]*(2 - self.u[self.n - 1])) # l_n
+        self.z[self.n] = (self.alphas[self.n] - self.h[self.n-1]*self.z[self.n - 1])/self.l[self.n] # z_n
 
         c = np.zeros((self.n+1,))
         c[self.n] = self.z[self.n]
@@ -47,12 +50,13 @@ class Spline:
         for j in range(self.n-1, -1, -1):
             self.c[j] = self.z[j] - (self.u[j]*self.c[j+1])
 
-            self.b[j] = (self.a[j + 1] - self.a[j])/self.h - (self.h*(self.c[j + 1] + 2*self.c[j]))/3
+            self.b[j] = (self.a[j + 1] - self.a[j])/self.h[j] - (self.h[j]*(self.c[j + 1] + 2*self.c[j]))/3
 
-            self.d[j] = (self.c[j + 1] - self.c[j])/(3*self.h)
+            self.d[j] = (self.c[j + 1] - self.c[j])/(3*self.h[j])
 
     def interpolate(self):
         self.a = self.y
+        self.calculate_h()
         self.calculate_alphas()
         self.solve_linear_system()
         self.finish_parameters()
@@ -61,7 +65,7 @@ class Spline:
         return self.a[j] + self.b[j]*(xi-self.x[j]) + self.c[j]*(xi-self.x[j])**2 + self.d[j]*(xi-self.x[j])**3
 
     def s(self, xi):
-        indice = ((xi - self.x[0]) // self.h).astype(int)
+        indice = ((xi - self.x[0]) // self.h[0]).astype(int)
         return self.sj(xi, indice)
 
     def plot(self, title="Interpolação por splines cúbicas", var_name='f',
